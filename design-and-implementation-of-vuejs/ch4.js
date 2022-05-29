948e25369dd4b8cba9d209e20049316305aee308
1-week <bayeszou@gmail.com>
2022-05-29 18:03:17 +0800

    4.4 cleanup 依赖

--- design-and-implementation-of-vuejs/ch4.js
+++ design-and-implementation-of-vuejs/ch4.js
@@ -1,7 +1,19 @@
 let activeEffect;
 
-function callEffect(effect) {
-  activeEffect = effect;
+function cleanup(effect) {
+  effect.deps.forEach(deps=>{
+    deps.delete(effect)
+  })
+  effect.deps.length = 0
+}
+
+function callEffect(fn) {
+  const effect = () => {
+    cleanup(effect)
+    activeEffect = effect;
+    fn();
+  };
+  effect.deps = []
   effect();
 }
 
@@ -9,7 +21,7 @@ function callEffect(effect) {
 let bucket = new WeakMap();
 
 let obj = new Proxy(
-  { text: 123 },
+  { text: 123, ok: true },
   {
     get(target, key) {
       track(target, key);
@@ -18,6 +30,9 @@ let obj = new Proxy(
     set(target, key, newVal) {
       target[key] = newVal;
       trigger(target, key);
+      // > The set() method should return a boolean value.
+      // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/Proxy/set#return_value
+      return true
     },
   }
 );
@@ -33,18 +48,33 @@ function track(target, key) {
     depsMap.set(key, (deps = new Set()));
   }
   deps.add(activeEffect);
+
+  // 副作用函数收集自己所访问的 property
+  activeEffect.deps.push(deps);
 }
 function trigger(target, key) {
   const depsMap = bucket.get(target);
   if (!depsMap) return;
   const deps = depsMap.get(key);
-  if (deps) deps.forEach((f) => f());
+  if (!deps) return
+  // NOTE: 避免无限循环
+  const effectsToRun = new Set(deps)
+  effectsToRun.forEach((f) => f());
 }
 
 callEffect(() => {
-  document.body.innerText = obj.text;
+  console.log('render')
+  document.body.innerText = obj.ok? obj.text: "it's not ok";
 });
 
 setTimeout(() => {
   obj.text = "eqpiruqpoiewu";
 }, 2000);
+
+setTimeout(() => {
+  obj.ok = false;
+}, 3000);
+
+setTimeout(() => {
+  obj.text = "123123";
+}, 4000);
