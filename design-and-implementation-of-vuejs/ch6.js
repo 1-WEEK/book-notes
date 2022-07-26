87ef8158245d4408f0aab81d34f72195749f8f86
1-week <bayeszou@gmail.com>
2022-07-26 18:40:50 +0800

    6.2 避免丢失响应

--- design-and-implementation-of-vuejs/ch6.js
+++ design-and-implementation-of-vuejs/ch6.js
@@ -15,4 +15,32 @@ export function ref(v) {
   return reactive(wrapper);
 }
 
+export function toRef(v, key) {
+  const wrapper = {
+    get value() {
+      return v[key];
+    },
+    set value(newV) {
+      v[key] = newV;
+    },
+    [IS_REF]: true,
+  };
+
+  return wrapper;
+}
+
+export function toRefs(obj) {
+  const wrap = {};
+  for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+      wrap[key] = toRef(obj, key);
+    }
+  }
+  return wrap;
+}
+
+export function proxyRefs(obj) {
+  return obj
+}
+
 export { callEffect, reactive };
--- design-and-implementation-of-vuejs/ch6.test.js
+++ design-and-implementation-of-vuejs/ch6.test.js
@@ -1,4 +1,5 @@
-import { ref, callEffect } from "./ch6";
+import { reactive } from "./ch5";
+import { ref, callEffect, toRef, toRefs, proxyRefs } from "./ch6";
 import { isRef } from "./_utils";
 import mockConsole from "jest-mock-console";
 
@@ -14,6 +15,55 @@ describe("ch6", () => {
     expect(console.log).nthCalledWith(1, 10086);
     a.value = 1024;
     expect(console.log).nthCalledWith(2, 1024);
+    restoreConsole();
+  });
+
+  it("toRef", () => {
+    const restoreConsole = mockConsole();
+    const a = reactive({
+      foo: 1,
+      bar: 2,
+    });
+    jest.spyOn(console, "log");
+    const newA = toRef(a, "foo");
+    expect(isRef(newA)).toBeTruthy();
+    callEffect(() => {
+      console.log(newA.value);
+    });
+    expect(console.log).nthCalledWith(1, 1);
+    newA.value = 123;
+    expect(console.log).nthCalledWith(2, 123);
+    restoreConsole();
+  });
+
+  it("toRefs", () => {
+    const restoreConsole = mockConsole();
+    jest.spyOn(console, "log");
+    const a = reactive({
+      foo: 1,
+      bar: 2,
+    });
+    const newA = toRefs(a);
+
+    callEffect(() => {
+      console.log(newA.foo.value, newA.bar.value);
+    });
+    expect(console.log).nthCalledWith(1, 1, 2);
+    newA.bar.value = 123;
+    expect(console.log).nthCalledWith(2, 1, 123);
+
+    restoreConsole();
+  });
+
+  it("unref", () => {
+    const restoreConsole = mockConsole();
+    jest.spyOn(console, "log");
+
+    callEffect(() => {
+      console.log();
+    });
+    expect(console.log).nthCalledWith(1, );
+
     restoreConsole();
   });
 });
