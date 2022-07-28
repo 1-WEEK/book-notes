ea7d585d3aae73849198359ff67257ca252031dd
1-week <bayeszou@gmail.com>
2022-07-28 18:54:41 +0800

    7.2 基本概念

--- design-and-implementation-of-vuejs/ch7.js
+++ design-and-implementation-of-vuejs/ch7.js
@@ -1,3 +1,12 @@
+/**
+ *
+ * @param {string} oldVnode
+ * @param {string} newVnode
+ * @param {Element} container
+ */
+function patch(oldVnode, newVnode, container) {
+  container.innerHTML = newVnode;
+}
 
 export function createRenderer() {
   /**
@@ -5,8 +14,19 @@ export function createRenderer() {
    * @param {string} domStr
    * @param {Element} container
    */
-  function render(domStr, container) {
-    container.innerHTML = domStr;
+  function render(vnode, container) {
+    if (vnode) {
+      if (container.__vnode) {
+        patch(container.__vnode, vnode, container);
+      } else {
+        // mount
+        container.innerHTML = vnode;
+      }
+    } else {
+      // unmount
+      if (container.__vnode) container.innerHTML = "";
+    }
+    container.__vnode = vnode;
   }
   return {
     render,
